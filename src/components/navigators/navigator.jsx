6b52299541c165d012d897a78667ba1846ae6ce1
odaydid002 <odaydid002.com>
2025-10-28 23:47:57 +0100

    project structur

--- src/components/navigators/navigator.jsx
+++ src/components/navigators/navigator.jsx
@@ -2,14 +2,14 @@ import NavElement from './navElement';
 import NavContainer from './navContainer';
 import NavElContainer from './navElementsContainer';
 
-function Navigator(props) {
+function Navigator() {
     return ( 
-        <NavContainer id={props.id} classNames={props.classNames}>
-            <NavElContainer navState={props.navState} title="Main">
+        <NavContainer classNames="ease-in-out pos-abs pos-center-v mrl">
+            <NavElContainer title="Main">
                 <NavElement onClick={()=>{alert("Home")}} id="el1" icon="fa-solid fa-house" title="Home"></NavElement>
                 <NavElement onClick={()=>{alert("Calendar")}} id="el2" icon="fa-regular fa-calendar" title="Calendar"></NavElement>
             </NavElContainer>
-            <NavElContainer navState={props.navState} title="System" classNames="mrta">
+            <NavElContainer title="System" classNames="mrta">
                 <NavElement onClick={()=>{alert("Settings")}} id="el2" icon="fa-solid fa-gear" title="Settings"></NavElement>
                 <NavElement onClick={()=>{alert("Notification")}} id="el2" icon="fa-regular fa-bell" title="Notification"></NavElement>
             </NavElContainer>
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import NavContainer from './components/navigators/navContainer'
-import NavElContainer from './components/navigators/navElementsContainer'
+import NavElement from './components/navigators/navElement';
+import NavContainer from './components/navigators/navContainer';
+import NavElContainer from './components/navigators/navElementsContainer';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NavContainer>
-      <NavElContainer>
-        
-      </NavElContainer>
-    </NavContainer>
+    <NavContainer classNames="ease-in-out pos-abs pos-center-v mrl">
+        <NavElContainer title="Main">
+            <NavElement onClick={()=>{alert("Home")}} id="el1" icon="fa-solid fa-house" title="Home"></NavElement>
+            <NavElement onClick={()=>{alert("Calendar")}} id="el2" icon="fa-regular fa-calendar" title="Calendar"></NavElement>
+        </NavElContainer>
+        <NavElContainer title="System" classNames="mrta">
+            <NavElement onClick={()=>{alert("Settings")}} id="el2" icon="fa-solid fa-gear" title="Settings"></NavElement>
+            <NavElement onClick={()=>{alert("Notification")}} id="el2" icon="fa-regular fa-bell" title="Notification"></NavElement>
+        </NavElContainer>
+    </NavContainer> 
   </StrictMode>,
 )
